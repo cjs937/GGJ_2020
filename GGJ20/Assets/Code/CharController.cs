e542b40b186fdf21d7738ad3557f92f22d08ef08
Adam Yoder <adam.christian.yoder@gmail.com>
2020-02-01 16:15:12 -0500

    added some rotation while turning

--- GGJ20/Assets/Code/CharController.cs
+++ GGJ20/Assets/Code/CharController.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharController : MonoBehaviour
 {
+
+	public GameObject playerVisual;
 	float verticalMoveSpeed = 0f;
 	float horizontalMoveSpeed = 0f;
 	const int maxSpeed = 25;
 	const int minSpeed = 0;
 
-	Vector3 forward, right;
+	Vector3 forward, right, rotateDir, rightMovement, upMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +30,10 @@ public class CharController : MonoBehaviour
 	void Move()
 	{
 		setMoveSpeed();
-		applyRotation();
-		applyMovement();
+        calcMovement();
+        applyMovement();
+        applyRotation();
+		
 		//moveSpeed += Input.GetAxis("LS_Vertical");
 
 		//if (moveSpeed > maxSpeed)
@@ -85,12 +89,10 @@ public class CharController : MonoBehaviour
 		else if (verticalMoveSpeed < minSpeed)
 			verticalMoveSpeed = minSpeed;
 	}
-    /*
+    
     void calcMovement()
 	{
-		Vector3 rightMovement = transform.right * verticalMoveSpeed * Time.deltaTime * Input.GetAxis("RS_Horizontal");
-
-		Vector3 upMovement;
+		rightMovement = transform.right * verticalMoveSpeed * Time.deltaTime * Input.GetAxis("RS_Horizontal");
 
 		if (Input.GetAxis("RS_Vertical") >= 0)
 
@@ -106,38 +108,23 @@ public class CharController : MonoBehaviour
 
 			upMovement = transform.forward * verticalMoveSpeed * Time.deltaTime;
 	}
-    */
+    
     void applyRotation ()
 	{
-		Vector3 rotateDir = transform.forward + transform.right * Time.deltaTime * Input.GetAxis("LS_Horizontal") * 5;
+		rotateDir = transform.forward + transform.right * Time.deltaTime * Input.GetAxis("LS_Horizontal") * 5;
 		transform.forward = rotateDir;
 
-		//float zRotation = -45f * Input.GetAxis("RS_Horizontal");
-		//float xRotation = 45f * Input.GetAxis("RS_Vertical");
+	    float zRotation = -45f * Input.GetAxis("RS_Horizontal");
+        //float xRotation = 45f * Input.GetAxis("RS_Vertical");
 
-		//transform.Rotate(0.0f, 0.0f, zRotation, Space.Self);
+        //playerVisual.transform.rotation = Quaternion.Euler(0.0f, 0.0f, zRotation);
+        //playerVisual.transform.rotation =
+        playerVisual.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0.0f, 0.0f, zRotation));
 		//transform.Rotate(xRotation, 0.0f, 0.0f, Space.Self);
 	}
 
     void applyMovement ()
 	{
-		Vector3 rightMovement = transform.right * verticalMoveSpeed * Time.deltaTime * Input.GetAxis("RS_Horizontal");
-
-		Vector3 upMovement;
-
-		if (Input.GetAxis("RS_Vertical") >= 0)
-
-			upMovement = transform.forward * (verticalMoveSpeed + (verticalMoveSpeed * Input.GetAxis("RS_Vertical"))) * Time.deltaTime;
-
-		//	Vector3 upMovement = transform.forward * (moveSpeed / 2) * Time.deltaTime;
-
-		else if (Input.GetAxis("RS_Vertical") < 0)
-
-			upMovement = transform.forward * (verticalMoveSpeed + ((verticalMoveSpeed * Input.GetAxis("RS_Vertical"))) / 2) * Time.deltaTime;
-
-		else
-
-			upMovement = transform.forward * verticalMoveSpeed * Time.deltaTime;
 		transform.position += rightMovement;
 		transform.position += upMovement;
 	}
