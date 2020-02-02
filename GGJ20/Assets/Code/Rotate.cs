7a28f88bcd54e9d8e892f61c5b6db3a6feac65e5
cjs937 <smithcory937@gmail.com>
2020-02-01 19:32:08 -0500

    Rotating lucy's fans

---
+++ GGJ20/Assets/Code/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public bool shouldRotate;
+    public float rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldRotate)
+            transform.Rotate(new Vector3(0, rotateSpeed, 0));
+    }
+}
