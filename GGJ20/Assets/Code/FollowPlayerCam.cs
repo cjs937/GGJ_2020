e5169dc52aa509985c6105be6e710a9cc175ed67
cjs937 <smithcory937@gmail.com>
2020-02-01 21:39:54 -0500

    Camera to follow the player

---
+++ GGJ20/Assets/Code/FollowPlayerCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCam : MonoBehaviour
+{
+    public float smoothingSpeed;
+    public Vector3 followOffset;
+    PlayerInteractionScript player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerInteractionScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player)
+        {
+            Vector3 lerpPos = player.followPos.position;
+            //lerpPos.y = transform.position.y;
+            lerpPos += followOffset;
+
+            transform.position = Vector3.Lerp(transform.position, lerpPos, smoothingSpeed * Time.deltaTime);
+            transform.LookAt(player.followPos);
+        }
+    }
+}
